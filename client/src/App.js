d6ec70968f4fd0566fc5682da3f78e9cfbaa1c85
Chandler Chilvers <cwchilvers@gmail.com>
2023-09-07 19:19:00 -0700

    Router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
+import Journal from './pages/Journal';
+import Gallery from './pages/Gallery';
+import WaysToHelp from './pages/WaysToHelp';
+import Planner from './pages/Planner';
+import Tributes from './pages/Tributes';
+import WellWishes from './pages/WellWishes';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Settings from './pages/Settings';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route 
+            path="/" 
+            element={<Home />} 
+          />
+          <Route
+            path="/journal"
+            element={<Journal />}
+          />
+          <Route 
+            path="/gallery"
+            element={<Gallery />}
+          />
+          <Route 
+            path="/ways-to-help"
+            element={<WaysToHelp />}
+          />
+          <Route
+            path="/planner"
+            element={<Planner />}
+          />
+          <Route 
+            path="/tributes"
+            element={<Tributes />}
+          />
+          <Route
+            path="/well-wishes"
+            element={<WellWishes />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route
+            path="/signup"
+            element={<Signup />}
+          />
+          <Route
+            path="/settings"
+            element={<Settings />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
