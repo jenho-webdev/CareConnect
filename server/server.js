49fa57c0a81925c0caca86bde3fdb36185300225
Jorge Duarte <sadujorge@gmail.com>
2023-09-07 17:28:37 -0700

    Setup express server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+// Import express module.
+const express = require('express')
+// Import path module.
+const path = require ('path')
+// Specify server listening port for production and development.
+const PORT = process.env.PORT || 3001;
+// Initialize a new instance of the Express application.
+const app = express();
+// Set up parsing middleware
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
