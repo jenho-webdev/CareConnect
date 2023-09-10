97d05e72578ca512dc80b0fe0162f16b7a032dcf
Jen Ho <jen@jenho.website>
2023-09-09 22:16:22 -0700

    Basic of the LoginForm layout done in tailwindcss, but still need formsubmission logic.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
 import { Image } from "@nextui-org/react";
-
+import LoginFrom from "./components/loginForm";
 import "./App.css";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <NextUIProvider>
       <div className="flex h-screen">
         {/* Left Column */}
-        <div className="flex-1 w-1/2 bg-gray-200">
+        <div className="flex-1 bg-gray-200">
           <div className="h-full flex items-center justify-center">
             <div className="max-w-md p-8 space-y-4 bg-white">
               {/* Logo */}
@@ -24,7 +24,11 @@ function App() {
         </div>
 
         {/* Right Column */}
-        <div className="lg:flex-1 bg-teal-400"></div>
+        <div className="flex-1 bg-teal-400">
+          <div className="h-full flex items-center justify-center">
+            <LoginFrom />
+        </div>
+      </div>
       </div>
     </NextUIProvider>
   );
