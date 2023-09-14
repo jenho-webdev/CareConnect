c9798e36d73f37f532d4658f53b2a27e71384569
Chandler Chilvers <cwchilvers@gmail.com>
2023-09-13 21:59:42 -0700

    Request Form

--- client/src/components/RequestForm.js
+++ client/src/components/RequestForm.js
@@ -4,13 +4,64 @@ const RequestForm = () => {
     render() {
         return (
             <div>
-                {/* TODO: 
-                    Title
-                    Description
-                    Start Time
-                    End Time
-                    Submit Button
-                */}
+                <form onSubmit={handleSubmit}>
+                    <h2>New Request</h2>
+                    <div>
+                        <div>
+                            <label htmlFor="title">
+                                Title
+                            </label>
+                            <input
+                                type="text"
+                                id="title"
+                                name="title"
+                                value={formData.title}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="Description">
+                                Description
+                            </label>
+                            <input
+                                type="text"
+                                id="description"
+                                name="description"
+                                value={formData.description}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="start-time">
+                                Start Time
+                            </label>
+                            <input
+                                type="text"
+                                id="start-time"
+                                name="start-time"
+                                value={formData.start-time}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="end-time">
+                                End Time
+                            </label>
+                            <input
+                                type="text"
+                                id="end-time"
+                                name="end-time"
+                                value={formData.end-time}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <button type="submit">
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         );
     }
