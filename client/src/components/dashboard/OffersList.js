9a2118f86523ec7d792d0f13a4868fb4d65cc3b6
Chandler Chilvers <cwchilvers@gmail.com>
2023-09-17 17:16:51 -0700

    Renaming files.

--- client/src/components/dashboard/OffersList.js
+++ client/src/components/dashboard/OffersList.js
@@ -3,21 +3,21 @@ import React from 'react';
 // Components
 import Offer from './Offer';
 
-const Offers = (props) => {
+const OffersList = (props) => {
     return (
         <div>
             {/* TODO: Pass in list title */}
             <h2>List Title</h2>
             <div>
                 {/* TODO: Pass in list title */}
-                <Offer />
-                <Offer />
-                <Offer />
-                <Offer />
-                <Offer />
+                <OffersList />
+                <OffersList />
+                <OffersList />
+                <OffersList />
+                <OffersList />
             </div>
         </div>
     );
 };
 
-export default Offers;
+export default OffersList;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 // Components
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Offers from '../components/dashboard/Offers';
+import OffersList from '../components/dashboard/OffersList';
 import Calendar from '../components/calendar/RequestsCalendar';
 
 const Dashboard = () => {
@@ -17,7 +17,7 @@ const Dashboard = () => {
             <Header />
             <main>
                 <h1>User Name</h1>
-                <Offers />
+                <OffersList />
                 <Calendar />
             </main>
             <Footer />
