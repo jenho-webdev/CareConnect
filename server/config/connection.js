1924266107e00a1408b297e49f636b0eb20e92da
Jorge Duarte <sadujorge@gmail.com>
2023-09-18 00:02:29 -0700

    Added connection to mongo Atlas database for production.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose');
+const path = require ('path')
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
+const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/careconnect') 
 
 module.exports = mongoose.connection;
