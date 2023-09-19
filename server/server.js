55613c418117f481fcf5508a00b41ba2e6ba2619
Jen Ho <jen@jenho.website>
2023-09-18 18:20:54 -0700

    SERVE REACT

--- server/server.js
+++ server/server.js
@@ -17,6 +17,13 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const startApolloServer = async () => {
   await server.start();
